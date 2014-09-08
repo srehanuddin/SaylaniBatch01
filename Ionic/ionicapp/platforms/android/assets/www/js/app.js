cc800d7631393a58cbd6b0eb8ab6f72ebecc4cfd
srehanuddin <s.rehanuddin@gmail.com>
2014-09-08 21:46:40 +0500

    Class 2014-09-07

--- Ionic/ionicapp/platforms/android/assets/www/js/app.js
+++ Ionic/ionicapp/platforms/android/assets/www/js/app.js
@@ -1,19 +1,78 @@
-// Ionic Starter App
+/*
+var app = angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: 'home.html'
+    })
+
+    $stateProvider.state('help', {
+        url: '/help',
+        templateUrl: 'help.html'
+    })
 })
+*/
+
+var app = angular.module('ionicApp', ['ionic'])
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('blank', {
+            url: '/blank',
+            views: {
+                'aaa' :{
+                    templateUrl: "blank.html"
+                }
+            }
+            //templateUrl: 'blank.html'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        })
+        .state('music', {
+            url: '/music',
+            templateUrl: 'music.html'
+        });
+    $urlRouterProvider.otherwise('/blank');
+})
+
+    .controller('MyController', function($scope, $ionicModal) {
+        $ionicModal.fromTemplateUrl('my-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+                $scope.modal = modal;
+            });
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+            // Execute action
+        });
+
+        setTimeout(function(){
+            $scope.openModal();
+        },3000 );
+    });
+
+
+function ContentController($scope, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+}
